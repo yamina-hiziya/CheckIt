9167342c5829815cb53eb5c9aa137b4569b16a8d
yamina <benhiziya7@gmail.com>
2025-10-25 16:49:15 +0200

    Remove demo.php from tracking

--- demo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//attention a ne pas utiliser ce code en production sans le securiser contre les injections SQL
-//n'est pas securiser contre les injections SQL
-
-$pdo = new PDO('mysql:dbname=studi_CheckIt;host=localhost;charset=utf8mb4', 'root', '');
-$id = $_GET['id'];
-$query = $pdo->query("SELECT * FROM user WHERE id = $id");
-$result = $query->fetch(PDO::FETCH_ASSOC);
