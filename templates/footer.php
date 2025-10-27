7e8ad98a1c7f17cab6a9b4fd83435eb95b105d38
yamina <benhiziya7@gmail.com>
2025-10-27 15:52:45 +0100

    feat modif footer lien listes

--- templates/footer.php
+++ templates/footer.php
@@ -2,8 +2,7 @@
     <footer class="py-3 my-4">
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
             <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+            <li class="nav-item"><a href="mes-listes.php" class="nav-link px-2">Mes listes</a></li>
             <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
             <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
         </ul>
