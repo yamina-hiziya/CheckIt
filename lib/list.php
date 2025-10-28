4e5081d7475f99b3386573fe488d3545746d8ba8
yamina <benhiziya7@gmail.com>
2025-10-28 20:26:39 +0100

    feat function getlistbyid

--- lib/list.php
+++ lib/list.php
@@ -10,6 +10,15 @@ function getListsByUserId(PDO $pdo, int $userId): array
     return $lists;
 }
 
+function getListById(PDO $pdo, int $id): array|bool
+{
+    $query = $pdo->prepare("SELECT * FROM list WHERE id = :id");
+    $query->bindValue(':id', $id, PDO::PARAM_INT);
+    $query->execute();
+
+    return $query->fetch(PDO::FETCH_ASSOC);
+}
+
 function saveList(PDO $pdo, string $title, int $user_id, int $categoryId, ?int $id = null): int|bool
 {
     if ($id) {
